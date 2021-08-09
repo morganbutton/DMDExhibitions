e7ba37f99ffe15d4065e216fe3a374e98a13445c
DESKTOP-65PKKFE\morga <morgan button>
2021-08-09 19:11:24 -0400

    removed all cache files from repo

--- cache/compiled/files/3046b40f21e878fe90fb28dc12857db3.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/var/www/html/user/config/security.yaml',
-    'modified' => 1625096747,
-    'data' => [
-        'salt' => 'RHfL5Fw6tu4vaE'
-    ]
-];
--- cache/compiled/files/faba32d5fd5452f9649b424896eb210b.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/var/www/html/user/config/media.yaml',
-    'modified' => 1626141697,
-    'data' => [
-        
-    ]
-];
